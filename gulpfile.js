961a41c92fd17552f7b05715a2a6da91a8e85e14
joeblotest <jason@oyster.local>
2014-10-16 07:27:12 +1100

    first

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
+
+var gulpif = require('gulp-if');
+var sprite = require('css-sprite').stream;
+
+gulp.task('sprites', function () {
+  return gulp
+  .src('./source_images/*.png')
+  .pipe(sprite({
+    retina: true,
+    margin: 20,
+    opacity: 0,
+    name: 'sprite',
+    style: '_sprite.scss',
+    cssPath: '../images',
+    processor: 'scss'
+  }))
+  .pipe(gulpif('*.png', gulp.dest('./built/images/'), gulp.dest('./built/styles/')))
+});
